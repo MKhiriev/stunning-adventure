8b6f9f3b52e74f6622386233e0988c8b53e8a6cd
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-07-13 17:23:14 +0500

    fixed poll counter

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -158,7 +158,6 @@ func (m *MetricsAgent) Run() error {
 }
 
 func (m *MetricsAgent) getSliceOfMetrics(memStats runtime.MemStats) []models.Metrics {
-	m.pollCount += 1
 	return []models.Metrics{
 		gaugeMetric("Alloc", float64(memStats.Alloc)),
 		gaugeMetric("BuckHashSys", float64(memStats.BuckHashSys)),
