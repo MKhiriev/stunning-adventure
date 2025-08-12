60782312b51da33bf2348b12b8b766cad0edbaad
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-08-12 17:42:38 +0500

    minor fix from db ping func

--- internal/handlers/ping_handler.go
+++ internal/handlers/ping_handler.go
@@ -3,12 +3,6 @@ package handlers
 import "net/http"
 
 func (h *Handler) Ping(w http.ResponseWriter, r *http.Request) {
-	// TODO decide how to fix redundant nil check
-	//if h.db == nil {
-	//	h.logger.Error().Str("func", "*Handler.Ping").Msg("db connection is nil")
-	//	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	//	return
-	//}
 	err := h.db.Ping(r.Context())
 	if err != nil {
 		h.logger.Err(err).Str("func", "*Handler.Ping").Msg("database ping error")
