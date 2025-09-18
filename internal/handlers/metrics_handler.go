300aa89cd0d1c44a2429d45aa9aa9af39921b31b
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-09-18 21:41:37 +0500

    fixed http status

--- internal/handlers/metrics_handler.go
+++ internal/handlers/metrics_handler.go
@@ -104,7 +104,7 @@ func (h *Handler) GetMetricJSON(w http.ResponseWriter, r *http.Request) {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
 			h.logger.Err(err).Caller().Str("func", "*Handler.GetMetricJSON").Any("metric to find", metric).Msg("metric not found")
-			http.Error(w, "metric not found", http.StatusBadRequest)
+			http.Error(w, "metric not found", http.StatusNotFound)
 			return
 		case errors.Is(err, validators.ErrEmptyID) || errors.Is(err, validators.ErrEmptyType) || errors.Is(err, validators.ErrInvalidType):
 			h.logger.Err(err).Caller().Str("func", "*Handler.GetMetricJSON").Any("metric to find", metric).Msg("metric type is not valid")
