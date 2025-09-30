1847d30804b1bb28c9b49929a1fba4eea1e98c7f
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-09-30 13:47:55 +0500

    added verbose logging fo cache metrics service

--- internal/service/cache_metrics_service.go
+++ internal/service/cache_metrics_service.go
@@ -20,20 +20,24 @@ func (c *CacheMetricsService) Save(ctx context.Context, metric models.Metrics) (
 	// save metric in cache memory
 	result, err := c.cache.Save(ctx, metric)
 	if err != nil {
+		c.log.Err(err).Str("func", "*CacheMetricsService.Save").Msg("error during saving metric in cache")
 		return models.Metrics{}, fmt.Errorf("error during saving metric in cache: %w", err)
 	}
 
 	// if file storage was provided
+	c.log.Debug().Str("func", "*CacheMetricsService.Save").Bool("file exists?", c.file != nil).Msg("checking if file exists")
 	if c.file != nil {
 		// get all metrics from Cache storage
 		allMetrics, err := c.cache.GetAll(ctx)
 		if err != nil {
+			c.log.Err(err).Str("func", "*CacheMetricsService.Save").Msg("error during getting all metrics from cache")
 			return models.Metrics{}, fmt.Errorf("error during getting all metrics from cache: %w", err)
 		}
 
 		// save all metrics to file
 		err = c.file.SaveAll(ctx, allMetrics)
 		if err != nil {
+			c.log.Err(err).Str("func", "*CacheMetricsService.Save").Msg("error during saving all metrics to file")
 			return models.Metrics{}, fmt.Errorf("error during saving all metrics to file: %w", err)
 		}
 	}
@@ -45,14 +49,17 @@ func (c *CacheMetricsService) SaveAll(ctx context.Context, metrics []models.Metr
 	// save all metrics in cache memory
 	err := c.cache.SaveAll(ctx, metrics)
 	if err != nil {
+		c.log.Err(err).Str("func", "*CacheMetricsService.SaveAll").Msg("error during saving all metrics in cache")
 		return fmt.Errorf("error during saving all metrics in cache: %w", err)
 	}
 
 	// if file storage was provided
+	c.log.Debug().Str("func", "*CacheMetricsService.SaveAll").Bool("file exists?", c.file != nil).Msg("checking if file exists")
 	if c.file != nil {
 		// save all provided metrics
 		err = c.file.SaveAll(ctx, metrics)
 		if err != nil {
+			c.log.Err(err).Str("func", "*CacheMetricsService.SaveAll").Msg("error during saving all metrics to file")
 			return fmt.Errorf("error during saving all metrics to file: %w", err)
 		}
 	}
