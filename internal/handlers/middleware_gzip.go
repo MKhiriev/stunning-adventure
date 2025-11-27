1e9a06b36c498015c547072aa8dcf4a3cc44bb49
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-11-28 03:26:20 +0500

    GZIP middleware: - fixed gzip reader and writer allocations for each request

--- internal/handlers/middleware_gzip.go
+++ internal/handlers/middleware_gzip.go
@@ -2,9 +2,24 @@ package handlers
 
 import (
 	"compress/gzip"
+	"io"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+var gzipWriterPool = sync.Pool{
+	New: func() any {
+		w := gzip.NewWriter(nil)
+		return w
+	},
+}
+
+var gzipReaderPool = sync.Pool{
+	New: func() any {
+		return new(gzip.Reader)
+	},
+}
 
 func GZip(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
@@ -15,51 +30,68 @@ func GZip(next http.Handler) http.Handler {
 		isGzipRequest := strings.Contains(contentEncoding, "gzip")
 
 		if isGzipRequest && req.Body != nil {
-			gzipReader, err := gzip.NewReader(req.Body)
-			if err != nil {
+			gzipReader := gzipReaderPool.Get().(*gzip.Reader)
+			if err := gzipReader.Reset(req.Body); err != nil {
+				gzipReaderPool.Put(gzipReader)
 				http.Error(w, "Invalid gzip data", http.StatusBadRequest)
 				return
 			}
-			defer gzipReader.Close()
 
-			req.Body = gzipReader
+			req.Body = &wrappedReadCloser{
+				Reader: gzipReader,
+				OnClose: func() {
+					gzipReader.Close()
+					gzipReaderPool.Put(gzipReader)
+				},
+			}
 			req.Header.Del("Content-Encoding")
 		}
 
-		if supportsGzip {
-			gzipWriter := gzip.NewWriter(w)
-			defer gzipWriter.Close()
-
-			gzipRW := &gzipResponseWriter{
-				ResponseWriter: w,
-				gzipWriter:     gzipWriter,
-				statusCode:     http.StatusOK,
-			}
-
-			next.ServeHTTP(gzipRW, req)
-		} else {
+		if !supportsGzip {
 			next.ServeHTTP(w, req)
+			return
 		}
+
+		gzipWriter := gzipWriterPool.Get().(*gzip.Writer)
+
+		gzipRW := &gzipResponseWriter{
+			ResponseWriter: w,
+			gzipWriter:     gzipWriter,
+		}
+
+		gzipWriter.Reset(w)
+
+		next.ServeHTTP(gzipRW, req)
+
+		gzipWriter.Close()
+		gzipWriterPool.Put(gzipWriter)
 	})
+}
+
+type wrappedReadCloser struct {
+	io.Reader
+	OnClose func()
+}
+
+func (w *wrappedReadCloser) Close() error {
+	if w.OnClose != nil {
+		w.OnClose()
+	}
+	return nil
 }
 
 type gzipResponseWriter struct {
 	http.ResponseWriter
-	gzipWriter  *gzip.Writer
-	statusCode  int
-	contentSize int
+	gzipWriter *gzip.Writer
 }
 
 func (w *gzipResponseWriter) WriteHeader(statusCode int) {
-	w.statusCode = statusCode
 	w.Header().Set("Content-Encoding", "gzip")
 	w.ResponseWriter.WriteHeader(statusCode)
 }
 
 func (w *gzipResponseWriter) Write(data []byte) (int, error) {
-	contentSize, err := w.gzipWriter.Write(data)
-	w.contentSize += contentSize
-	return contentSize, err
+	return w.gzipWriter.Write(data)
 }
 
 func (w *gzipResponseWriter) Close() error {
