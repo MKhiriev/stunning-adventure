636250a3f000caa87770d65d667ef28b444b2b89
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-11-28 03:14:47 +0500

    WithLogging middleware: - deleted allocation of `responseData` from loggingResponseWriter for every request - deleted http-body copying for every request

--- internal/handlers/middleware_logging.go
+++ internal/handlers/middleware_logging.go
@@ -1,78 +1,57 @@
 package handlers
 
 import (
-	"bytes"
-	"io"
 	"net/http"
 	"time"
 )
 
 func (h *Handler) WithLogging(handler http.Handler) http.Handler {
 	logFn := func(w http.ResponseWriter, r *http.Request) {
-		h.logger.Info().
-			Any("incoming request headers", r.Header).
-			Msg("request")
 		start := time.Now()
 
 		uri := r.RequestURI
 		method := r.Method
 
-		responseData := &responseData{
-			status: 0,
-			size:   0,
-		}
-		lw := loggingResponseWriter{
+		lw := &loggingResponseWriter{
 			ResponseWriter: w,
-			responseData:   responseData,
+			status:         http.StatusOK,
 		}
 
-		handler.ServeHTTP(&lw, r)
+		handler.ServeHTTP(lw, r)
 
 		duration := time.Since(start)
-
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			h.logger.Err(err).Str("func", "*Handler.WithHashing").Msg("failed to read request body")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		r.Body = io.NopCloser(bytes.NewReader(body))
 
 		h.logger.Info().
 			Str("uri", uri).
 			Str("method", method).
-			Bytes("body", lw.responseData.body).
-			Any("headers", lw.Header()).
-			Int("status", lw.responseData.status).
+			Int("status", lw.status).
 			Dur("duration", duration).
-			Int("size", lw.responseData.size).
-			Msg("")
+			Int("size", lw.size).
+			Send()
 	}
 
 	return http.HandlerFunc(logFn)
 }
 
-type (
-	responseData struct {
-		status int
-		size   int
-		body   []byte
-	}
+type responseData struct {
+	status int
+	size   int
+	body   []byte
+}
 
-	loggingResponseWriter struct {
-		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
-		responseData        *responseData
-	}
-)
+type loggingResponseWriter struct {
+	http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+	status              int
+	size                int
+}
 
 func (r *loggingResponseWriter) Write(b []byte) (int, error) {
-	r.responseData.body = b
 	size, err := r.ResponseWriter.Write(b)
-	r.responseData.size += size
+	r.size += size
 	return size, err
 }
 
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.status = statusCode
 	r.ResponseWriter.WriteHeader(statusCode)
-	r.responseData.status = statusCode
 }
