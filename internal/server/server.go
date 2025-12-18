7d9f2ea1db7cad4e338dbdde92db7919814c8475
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-12-18 21:51:47 +0500

    server: added graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/MKhiriev/stunning-adventure/internal/config"
@@ -21,5 +26,27 @@ func (s *Server) ServerRun(handler http.Handler, cfg *config.ServerConfig) error
 		WriteTimeout: timeout,
 	}
 
-	return s.server.ListenAndServe()
+	idleConnsClosed := make(chan struct{})
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	go func() {
+		<-sigint
+
+		if err := s.server.Shutdown(context.Background()); err != nil {
+			// ошибки закрытия Listener
+			fmt.Printf("HTTP server Shutdown: %v\n", err)
+		}
+
+		close(idleConnsClosed)
+	}()
+
+	if err := s.server.ListenAndServe(); err != nil {
+		fmt.Printf("HTTP server ListenAndServe: %v\n", err)
+	}
+
+	<-idleConnsClosed
+	fmt.Println("Server Shutdown gracefully")
+
+	return nil
 }
